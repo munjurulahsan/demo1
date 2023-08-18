01443e34ab90c277d736d229fdf367c340092d6d
munjurulahsan <munjurulahsan@gmail.com>
2023-08-18 22:44:08 +0600

    This is version1

---
+++ 1_Swap_Number_without_third_veriable.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int number1,number2;
+    printf("Enter the value of Number1 :");
+    scanf("%d",&number1);
+    printf("Enter the value of Number2 :");
+    scanf("%d",&number2);
+    printf("Before Swapping Number: %d %d\n",number1,number2);
+
+    //Swapping
+    number1=number1+number2;
+    number2=number1-number2;
+    number1=number1-number2;
+    printf("After swapping: %d %d",number1,number2);
+    return 0;
+}
